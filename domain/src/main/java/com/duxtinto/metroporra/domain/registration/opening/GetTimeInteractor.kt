10bdbe9a9bf4f52fd9bce253a45fbaa70ff74439
duxtinto <duxtinto@gmail.com>
2017-07-09 17:00:01 +0700

    Simplifying rx test

--- domain/src/main/java/com/duxtinto/metroporra/domain/registration/opening/GetTimeInteractor.kt
+++ domain/src/main/java/com/duxtinto/metroporra/domain/registration/opening/GetTimeInteractor.kt
@@ -1,10 +1,11 @@
 package com.duxtinto.metroporra.domain.registration.opening
 
+import com.duxtinto.metroporra.data.registration.RegistrationInformationProvider
 import io.reactivex.Single
 import java.time.LocalDate
 
-internal class GetTimeInteractor {
+internal class GetTimeInteractor(val registrationInformation: RegistrationInformationProvider) {
     fun execute() : Single<LocalDate> {
-
+        return registrationInformation.getOpeningDate()
     }
 }
--- domain/src/test/java/com/duxtinto/metroporra/domain/registration/opening/GetTimeInteractorTest.kt
+++ domain/src/test/java/com/duxtinto/metroporra/domain/registration/opening/GetTimeInteractorTest.kt
@@ -2,28 +2,28 @@ package com.duxtinto.metroporra.domain.registration.opening
 
 import com.duxtinto.metroporra.data.registration.RegistrationInformationProvider
 import io.kotlintest.matchers.beOfType
-import io.kotlintest.matchers.beTheSameInstanceAs
 import io.kotlintest.matchers.should
 import io.kotlintest.mock.`when`
 import io.reactivex.Single
-import io.reactivex.schedulers.TestScheduler
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 import java.time.LocalDate
 
 internal class GetTimeInteractorTest {
 
     @Mock
-    lateinit var registrationInformation : RegistrationInformationProvider
-
-    val scheduler : TestScheduler = TestScheduler()
+    lateinit var registrationInformation: RegistrationInformationProvider
 
     lateinit var sutInteractor: GetTimeInteractor
 
     @BeforeEach
     fun setUp() {
-        sutInteractor = GetTimeInteractor()
+        MockitoAnnotations.initMocks(this)
+
+        sutInteractor = GetTimeInteractor(registrationInformation)
     }
 
     @Test
@@ -33,17 +33,17 @@ internal class GetTimeInteractorTest {
 
     @Test
     fun execute_ifOpeningPeriodIsAvailable_shouldEmitIt() {
-        val expectedOpeningDate = LocalDate.of(20, 11, 2017)
+        val expectedOpeningDate = LocalDate.of(2017, 11, 20)
         `when`(registrationInformation.getOpeningDate())
                 .thenReturn(Single.just(expectedOpeningDate))
 
-        val result : Single<LocalDate> = sutInteractor.execute()
+        val testObserver = sutInteractor.execute().test()
 
-        result
-                .subscribeOn(scheduler)
-                .observeOn(scheduler)
-                .doOnSuccess { date : LocalDate -> date should beTheSameInstanceAs(expectedOpeningDate) }
-
-        scheduler.triggerActions()
+        testObserver.awaitTerminalEvent()
+        testObserver
+                .assertNoErrors()
+                .assertValueCount(1)
+                .assertValue(expectedOpeningDate)
+        Mockito.verify(registrationInformation).getOpeningDate()
     }
 }
