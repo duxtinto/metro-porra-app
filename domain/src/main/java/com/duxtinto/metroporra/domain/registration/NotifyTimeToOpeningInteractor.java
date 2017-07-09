f6331a4e6534ea999404ffee24633937cc62f54b
duxtinto <duxtinto@gmail.com>
2017-07-09 09:30:33 +0700

    Transforming the classes into Kotlin

--- domain/src/main/java/com/duxtinto/metroporra/domain/registration/NotifyTimeToOpeningInteractor.java
+++
@@ -1,4 +0,0 @@
-package com.duxtinto.metroporra.domain.registration;
-
-class NotifyTimeToOpeningInteractor {
-}
---
+++ domain/src/main/java/com/duxtinto/metroporra/domain/registration/NotifyTimeToOpeningInteractor.kt
@@ -0,0 +1,3 @@
+package com.duxtinto.metroporra.domain.registration
+
+internal class NotifyTimeToOpeningInteractor
